b4440c286a361bbf67534fc5ca4ad7261b6d158a
翼云 <yiyunhuang1994@aliyun.com>
2020-05-25 23:08:06 +0800

    Initial commit

---
+++ src/com/yiyun/test/Test.java
@@ -0,0 +1,33 @@
+package com.yiyun.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 翼云
+ * @version 1.0
+ * @ClassName Test
+ * @Description  test class
+ * @program: java8SourceLearn
+ * @date 2020/5/25 21:26
+ **/
+
+public class Test {
+    public static void main(String[] args) {
+        /**
+         * io
+         * lang
+         * math
+         * net
+         * nio
+         * time
+         * util
+         * */
+        Map<String,Double> hashmap= new HashMap<>();
+
+        hashmap.put("K1",0.1);
+        hashmap.put("K2",0.2);
+
+
+    }
+}
